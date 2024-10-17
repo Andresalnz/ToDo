1f57ca7e7cb06e8ae930dabcfaf0492ca16c458e
Andresalnz <andresaleunz@gmail.com>
2024-10-17 11:35:38 +0200

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Andres Aleu on 17/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
