91673a97ecaae42041cc158c879a4ff2b04cbd31
Andresalnz <andresaleunz@gmail.com>
2024-10-22 13:02:45 +0200

    Add Presenter home

---
+++ NotesApp/ListOfTasks/ListOfTasksPresenter.swift
@@ -0,0 +1,53 @@
+//
+//  ListOfTasksPresenter.swift
+//  NotesApp
+//
+//  Created by Andres Aleu on 17/10/24.
+//
+
+import Foundation
+
+
+enum StatusNotes {
+    case loading
+    case loaded
+    case error
+}
+
+class ListOfTasksPresenter {
+    
+     let dataProvider: DataProvider?
+     var ui: DataProviderUi?
+    
+    var tasks: [TaskList]
+    
+    var status: ((StatusNotes) -> Void)?
+    
+    init(dataProvider: DataProvider?, tasks: [TaskList] = []) {
+        self.dataProvider = dataProvider
+        self.tasks = tasks
+       
+    }
+    
+    func viewDidLoad() {
+        fetchTasks()
+        
+    }
+    
+    func fetchTasks() {
+        do {
+            let data = try dataProvider?.fetchTasks()
+            tasks = data ?? []
+            
+            
+        } catch let err {
+            print("Error pintando toda las tareas \(err.localizedDescription)")
+            //Alerta
+        }
+    }
+    
+    func deleteTask(at index: Int) throws {
+       try dataProvider?.delete(task: tasks[index])
+        ui?.update()
+    }
+}
