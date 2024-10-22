eb2ccf9e98a5d9fce8244cd7b0b45c45e393e12c
Andresalnz <andresaleunz@gmail.com>
2024-10-22 13:04:27 +0200

    Add Presenter and viewController Add note

---
+++ NotesApp/AddTask/AddTaskPresenter.swift
@@ -0,0 +1,30 @@
+//
+//  AddTaskPresenter.swift
+//  NotesApp
+//
+//  Created by Andres Aleu on 17/10/24.
+//
+
+import Foundation
+import CoreData
+
+protocol AddTaskPresenterProtocol: AnyObject {
+    func addTask(name: String?, description: String?)
+}
+
+class AddTaskPresenter {
+    
+    
+    var interactor: DataProvider = DataProvider()
+    
+    init(interactor: DataProvider) {
+        self.interactor = interactor
+    }
+    
+    func addTask(name: String?, description: String?) throws {
+    
+          
+        try interactor.addTask(type: NSSQLiteStoreType, title: name ?? "" , detail: description ?? "")
+        
+    }
+}
--- NotesApp/AddTask/AddTaskViewController.swift
+++ NotesApp/AddTask/AddTaskViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class AddTaskViewController: UIViewController {
+   
+    
     
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var descriptionTextField: UITextField!
     @IBOutlet weak var saveButton: UIButton!
     
+    var presenter: AddTaskPresenter?
+    var ui: DataProviderUi?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +26,7 @@ class AddTaskViewController: UIViewController {
     }
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+       
     }
     
     required init?(coder: NSCoder) {
@@ -29,7 +34,26 @@ class AddTaskViewController: UIViewController {
     }
     
     
+    
+    
     @IBAction func saveButtonAction(_ sender: Any) {
+        
+        do {
+            if let name = nameTextField.text, !name.isEmpty, let description = descriptionTextField.text, !description.isEmpty {
+                try presenter?.addTask(name: nameTextField.text, description: descriptionTextField.text)
+                navigationController?.popViewController(animated: true)
+                ui?.update()
+            } else {
+                print("Vacio") //Alerta
+            }
+           
+        } catch let err {
+            print("Error en guardar \(err.localizedDescription)")
+            //alerta
+        }
+
+        
+      
     }
     
     /*
