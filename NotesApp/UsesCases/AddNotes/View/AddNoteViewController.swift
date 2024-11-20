36e7a950c5b49ac2d4ca60beb40a94b557794a9b
Andresalnz <andresaleunz@gmail.com>
2024-11-20 14:51:50 +0100

    Remove else

--- NotesApp/UsesCases/AddNotes/View/AddNoteViewController.swift
+++ NotesApp/UsesCases/AddNotes/View/AddNoteViewController.swift
@@ -83,12 +83,11 @@ class AddNoteViewController: UIViewController {
     
     @objc func doneButtonAction() {
         do {
+            
             if let title = titleTextField.text, !title.isEmpty, let description = descriptionTextView.text {
                 try presenter?.addNote(title: title, descriptionNote: description, date: .now)
                 navigationController?.popViewController(animated: true)
                 ui?.update()
-            } else {
-                print("Vacio") //Alerta
             }
             
         } catch {
